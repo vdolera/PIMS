22b49482b20bbf978a99e23fee73d8bf3b10faa4
YuanMLP <158263091+YuanMLP@users.noreply.github.com>
2025-05-12 22:14:03 +0800

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,6 +4,7 @@ import './HomePage.css';
 import Layout from '../Components/Layout';
 
 export default function Home() {
+  document.title = "PIMS | Home";
   const navigate = useNavigate();
   const [items, setItems] = useState([]);
   const [form, setForm] = useState({
